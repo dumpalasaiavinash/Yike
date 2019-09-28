df74f44b0a06fa8c6b22e2a7dcd4e49c47597309
dumpalasaiavinash <36899352+dumpalasaiavinash@users.noreply.github.com>
2019-09-28 23:50:54 +0530

    Av1

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,2 @@
 from django.urls import path, include
 from . import views
-
-app_name = 'home'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-]
