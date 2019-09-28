f58d11f23ee2335787a6ecb002b211ab3fb8616d
Anirudh Ambati <anirudhambati@gmail.com>
2019-09-28 18:54:13 +0530

    login/signup frontend

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
