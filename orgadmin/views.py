4105454cf415c4a330f4764709cec4be7baf1c50
Yashwanth_11 <yashukikkuri@gmail.com>
2019-10-17 13:23:13 +0530

    avinash and pranay's updates on orgadmin

--- orgadmin/views.py
+++ orgadmin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    return render(request, 'dashboard/index.html')
 
 def create(request):
     return render(request, 'orgadmin/create.html')
