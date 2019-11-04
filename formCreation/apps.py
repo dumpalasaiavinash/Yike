ba51c88a04b2be7c22ed72a34300e2b8fcd1a604
avinashkatariya <44751905+avinashkatariya@users.noreply.github.com>
2019-11-04 21:13:05 +0530

    update

---
+++ formCreation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FormcreationConfig(AppConfig):
+    name = 'formCreation'
---
+++ formCreation/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def formCreateMain(req):
+    return render(req,"formCreation/form_main.html")
+# Create your views here.
