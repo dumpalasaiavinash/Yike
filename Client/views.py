e471d3bc6b663fa43eab99db755086a6e84223b7
abhishek <Abhi@gmail.com>
2019-11-15 20:51:52 +0530

    client_email_verification

--- Client/views.py
+++ Client/views.py
@@ -6,6 +6,28 @@ from boto3.dynamodb.conditions import Key, Attr
 from datetime import date
 import re,hashlib
 
+#install all of these
+#pip install ndg-httpsclient
+#pip install pyopenssl
+#pip install pyasn1
+
+
+#For sending activation function
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+import datetime
+
+#For generating token
+import random
+import string
+
 
 # Create your views here.
 def firstpage(request):
@@ -56,7 +78,6 @@ def complaint(request):
         return render(request,'Client/second.html')
 
 def client_signup(request):
-
     return render(request,'Client/client_signup.html')
 
 def client_signin(request):
@@ -74,17 +95,20 @@ def client_signin(request):
 
         #mail validation
         regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
         if(re.search(regex,email)):
             m = 1
             print('VALID EMAIL')
+            print(context)
         else:
             m=0
         #username validation
-        if(m==1):
+        if(m==1 and context != []):
             for con in context:
                 print(con['username'],username)
                 if(con['username']==username or con['email']==email):
                     print('yes')
+                    print(con['username'],username,con['email'],email)
                     return render(request,'Client/client_signup.html')
                 else:
                      if(password==repassword):
@@ -92,19 +116,90 @@ def client_signin(request):
                         password_hashed = hashlib.sha256(password.encode())
                         password_hashed = password_hashed.hexdigest()
                         print(password_hashed)
+                        letters=string.ascii_letters
+                        token=''.join(random.choice(letters) for i in range(10))
+                        client_id=len(response['Items'])+1
                         table.put_item(
                             Item={
-                                'client_id':len(response['Items'])+1,
+                                'client_id':client_id,
                                 'username':username,
                                 'email':email,
                                 'password':password_hashed,
+                                'active':False,
+                                'token':token,
                                 }
-
                         )
+                        current_site = get_current_site(request)
+                        mail_subject = 'Email COnfirmation'
+                        message = render_to_string('Client/acc_active_email.html', {
+                            'user': username,
+                            'user_email':email,
+                            'domain': current_site.domain,
+                            'token':token,
+                            'client_id':client_id
+                        })
+                        to_email = email
+                        email = EmailMessage(
+                                    mail_subject, message, to=[to_email]
+                        )
+                        email.send()
                         return render(request,'Client/client_signed.html')
-
-
+        else:
+            if(password==repassword):
+                        print(password)
+                        password_hashed = hashlib.sha256(password.encode())
+                        password_hashed = password_hashed.hexdigest()
+                        print(password_hashed)
+                        letters=string.ascii_letters
+                        token=''.join(random.choice(letters) for i in range(10))
+                        client_id=len(response['Items'])+1
+                        table.put_item(
+                            Item={
+                                'client_id':client_id,
+                                'username':username,
+                                'email':email,
+                                'password':password_hashed,
+                                'active':False,
+                                'token':token,
+                                }
+                        )
+                        current_site = get_current_site(request)
+                        mail_subject = 'Email COnfirmation'
+                        message = render_to_string('Client/acc_active_email.html', {
+                            'user': username,
+                            'user_email':email,
+                            'domain': current_site.domain,
+                            'token':token,
+                            'client_id':client_id
+                        })
+                        to_email = email
+                        email = EmailMessage(
+                                    mail_subject, message, to=[to_email]
+                        )
+                        email.send()
+                        return render(request,'Client/client_signed.html')
     return render(request,'Client/client_signup.html')
+
+def activate(request,token,email,username,client_id):
+    dynamodb=boto3.resource('dynamodb')
+    table=dynamodb.Table('clients')
+
+    response=table.scan()
+
+    for cli in response['Items']:
+        if (cli['client_id']==int(client_id)) and (cli['token']==token):
+            table.update_item(
+                    Key={
+                        'client_id':cli['client_id']
+                    },
+                    UpdateExpression="set active = :r",
+                    ExpressionAttributeValues={
+                        ':r':True
+                    }
+                )
+            return render(request,'Client/client_verify.html')
+
+
 
 
 def client_login(request):
@@ -116,7 +211,7 @@ def client_loggedin(request):
         table=dynamodb.Table('clients')
         response = table.scan()
         context = response['Items']
-        email    = request.POST.get('email')
+        email   = request.POST.get('email')
         password = request.POST.get('psw')
         password_hashed = hashlib.sha256(password.encode())
         password_hashed = password_hashed.hexdigest()
@@ -126,6 +221,5 @@ def client_loggedin(request):
                 return render(request,'Client/client_loggedin.html')
     return render(request,'Client/client_login.html')
 
-
-
-    
+def email_verification(request):
+    return render(request,'Client/client_verify.html')
