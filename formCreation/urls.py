b9ed5fbf430ecc0d48ab4d96027133d4c5eed615
Yashwanth_11 <yashukikkuri@gmail.com>
2019-11-16 11:48:25 +0530

    updated complaint form

--- formCreation/urls.py
+++ formCreation/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'form'
 
 urlpatterns = [
-    path('main/',views.formCreateMain),
-    path('test/',views.testF)
+    path('main/',views.formCreateMain, name='form_main'),
+    path('test/',views.testF, name='form_test')
 ]
